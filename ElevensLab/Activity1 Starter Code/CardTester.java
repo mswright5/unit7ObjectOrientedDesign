e2082b6a5cdde78ccd993b5e4f60be9a659939e0
mswright5 <matthew181132@yahoo.com>
2015-02-20 12:21:03 -0600

    Activity 2
    on the test class

--- ElevensLab/Activity1 Starter Code/CardTester.java
+++ ElevensLab/Activity1 Starter Code/CardTester.java
@@ -8,6 +8,34 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("Jack","Hearts", 11);
+		Card card2 = new Card("Jack","Hearts", 11);
+		Card card3 = new Card("Not a Jack", "Hearts", 11);
+		
+		System.out.println(card1);
+		System.out.println(card2);
+		System.out.println(card3);
+		
+		System.out.println("\nCard 1");
+		System.out.println(card1.suit());
+		System.out.println(card1.rank());
+		System.out.println(card1.pointValue());
+		
+		System.out.println("\nCard 2");
+		System.out.println(card2.suit());
+		System.out.println(card2.rank());
+		System.out.println(card2.pointValue());
+		
+		System.out.println("\nCard 3");
+		System.out.println(card3.suit());
+		System.out.println(card3.rank());
+		System.out.println(card3.pointValue());
+		
+		System.out.println();
+		
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card1));
+		System.out.println(card2.matches(card3));
+		System.out.println(card3.matches(card1));
 	}
 }
